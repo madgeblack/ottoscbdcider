f972f5e058e4bf86dd3436cee289ab948be2107e
Maddie Black <mblack@sockeye.com>
2020-06-15 16:54:49 -0700

    nutrition panel fixes and design tweaks post-launch

--- ottos/template-parts/flavors.php
+++ ottos/template-parts/flavors.php
@@ -4,24 +4,24 @@
 		
 		<div class="flavor">
 			<button class="accordion" id="pineapple">
-				<img class="" srcset="
+				<img class="flavor-name" srcset="
 					<?php echo get_template_directory_uri() . '/assets/flavor-pineapple.png' ?> 2x,
 					<?php echo get_template_directory_uri() . '/assets/flavor-pineapple-2x.png' ?> 1x"
 					src="<?php echo get_template_directory_uri() . '/assets/flavor-pineapple.png' ?>">
 			</button>
 			<div class="panel">
 					<p class="ingredients">
-					<span class="medium">INGREDIENTS:</span> Filtered Water, Organic Apple Juice Concentrate, Pineapple Juice Concentrate, Passionfruit Juice Concentrate, Citric Acid, Natural Hop Flavor, Hemp Extract (CBD), Potassium Sorbate.
+					<span class="medium">INGREDIENTS:</span> Filtered Water, Organic Apple Juice Concentrate, Pineapple Juice Concentrate, Passionfruit Juice Concentrate, Citric Acid, Natural Hop Flavor, Hemp Extract (CBD).
 					</p>
 
 					<p class="nutrition">
-						CALORIES: <span class="bold">80</span><br>
+						CALORIES: <span class="bold">110</span><br>
 						TOTAL FAT: <span class="bold">0g</span><br>
-						SODIUM: <span class="bold">15mg</span><br>
-						TOTAL CARBS: <span class="bold">20g</span><br>
-						TOTAL SUGARS: <span class="bold">19g</span><br>
-						PROTEIN: <span class="bold">0</span><br>
-						POTASSIUM: <span class="bold">270mg</span><br>
+						SODIUM: <span class="bold">20mg</span><br>
+						TOTAL CARBS: <span class="bold">25g</span><br>
+						TOTAL SUGARS: <span class="bold">24g</span><br>
+						PROTEIN: <span class="bold">0g</span><br>
+						POTASSIUM: <span class="bold">330mg</span><br>
 					</p>
 			</div> <!-- end panel -->
 		</div> <!-- end flavor -->
@@ -29,14 +29,14 @@
 
 		<div class="flavor">
 			<button class="accordion" id="apple">
-				<img class="" srcset="
+				<img class="flavor-name" srcset="
 					<?php echo get_template_directory_uri() . '/assets/flavor-apple.png' ?> 2x,
 					<?php echo get_template_directory_uri() . '/assets/flavor-apple-2x.png' ?> 1x"
 					src="<?php echo get_template_directory_uri() . '/assets/flavor-apple.png' ?>">
 			</button>
 			<div class="panel">
 					<p class="ingredients">
-					<span class="medium">INGREDIENTS:</span> Water, Organic Apple Juice Concentrate, Citric acid, Natural Hop Flavor, Hemp Extract (CBD), Potassium Sorbate.
+					<span class="medium">INGREDIENTS:</span> Filtered Water, Organic Apple Juice Concentrate, Citric Acid, Natural Hop Flavor, Hemp Extract (CBD).
 					</p><br>
 
 					<p class="nutrition">
@@ -45,7 +45,7 @@
 						SODIUM: <span class="bold">15mg</span><br>
 						TOTAL CARBS: <span class="bold">21g</span><br>
 						TOTAL SUGARS: <span class="bold">20g</span><br>
-						PROTEIN: <span class="bold">0</span><br>
+						PROTEIN: <span class="bold">0g</span><br>
 						POTASSIUM: <span class="bold">290mg</span><br>
 					</p>
 			</div> <!-- end panel -->
@@ -54,7 +54,7 @@
 		<div class="flavor">
 
 			<button class="accordion active" id="pear">
-				<img class="" srcset="
+				<img class="flavor-name" srcset="
 					<?php echo get_template_directory_uri() . '/assets/flavor-pear.png' ?> 2x,
 					<?php echo get_template_directory_uri() . '/assets/flavor-pear-2x.png' ?> 1x"
 					src="<?php echo get_template_directory_uri() . '/assets/flavor-pear.png' ?>">
@@ -62,17 +62,17 @@
 
 			<div class="panel default" id="pearPanel">
 					<p class="ingredients">
-					<span class="medium">INGREDIENTS:</span> Filtered Water, Organic Apple Juice Concentrate, Rhubarb Juice Concentrate, Pear Juice Concentrate, Citric Acid, Natural Hop Flavor, Hemp Extract (CBD) Potassium Sorbate (Preservative).
+					<span class="medium">INGREDIENTS:</span> Filtered Water, Organic Apple Juice Concentrate, Rhubarb Juice Concentrate, Pear Juice Concentrate, Citric Acid, Natural Hop Flavor, Hemp Extract (CBD).
 					</p>
 
 					<p class="nutrition">
-						CALORIES: <span class="bold">100</span><br>
+						CALORIES: <span class="bold">90</span><br>
 						TOTAL FAT: <span class="bold">0g</span><br>
-						SODIUM: <span class="bold">20mg</span><br>
-						TOTAL CARBS: <span class="bold">24g</span><br>
-						TOTAL SUGARS: <span class="bold">22g</span><br>
-						PROTEIN: <span class="bold">0</span><br>
-						POTASSIUM: <span class="bold">380mg</span><br>
+						SODIUM: <span class="bold">15mg</span><br>
+						TOTAL CARBS: <span class="bold">19g</span><br>
+						TOTAL SUGARS: <span class="bold">18g</span><br>
+						PROTEIN: <span class="bold">0g</span><br>
+						POTASSIUM: <span class="bold">310mg</span><br>
 					</p>
 			</div> <!-- end panel -->
 		</div> <!-- end flavor -->
